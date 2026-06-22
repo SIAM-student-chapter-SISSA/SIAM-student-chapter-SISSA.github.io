992d8d9f496a56b4c57129aea0e29711a885f818
cyberguli <guglielmo.padula@aruba.it>
2026-06-22 15:21:45 +0200

    fixed double seminar

--- assets/js/upcoming-seminars.js
+++ assets/js/upcoming-seminars.js
@@ -13,24 +13,4 @@
 // }
 
 window.UPCOMING_SEMINARS = [
-  {
-    day: "26",
-    month: "Jun",
-    year: "2026",
-    title: "Polytopal Discontinuous Galerkin Discretizations of Coupled Non-Newtonian Stokes–Darcy Systems",
-    speaker: "Valentina Pederzoli",
-    university: "Politecnico di Milano",
-  },
-  // {
-  //   day: "DD",
-  //   month: "MMM",
-  //   year: "YYYY",
-  //   title: "Seminar title",
-  //   speaker: "First Last",
-  //   university: "University name",
-  //   website: {
-  //     href: "https://...",
-  //     label: "Link label"
-  //   },
-  // }
 ];
