abbb59bde6b8ad265ce8e57c2409340f0b67dea9
ICGonnella <igonnell@mymate.>
2026-08-25 12:10:20 +0200

    update img and upcoming seminars

--- assets/js/past-ajs-seminars.js
+++ assets/js/past-ajs-seminars.js
@@ -7,6 +7,13 @@
 
 window.PAST_AJS_SEMINARS = [
   {
+    day: "26",
+    month: "Jun",
+    title: "Valentina Pederzoli (Politecnico di Milano)",
+    meta: "Fri 26/06/2026, 14:00 UTC",
+    website: { href: "https://researchseminars.org/seminar/AnJunSem", label: "researchseminars.org" },
+  },
+  {
     day: "19",
     month: "Jun",
     title: "Jan Zavodnik (University of Ljubljana)",
@@ -196,4 +203,3 @@ window.PAST_AJS_SEMINARS = [
     website: { href: "https://researchseminars.org/seminar/AnJunSem", label: "researchseminars.org" },
   },
 ];
-
--- assets/js/upcoming-ajs-seminars.js
+++ assets/js/upcoming-ajs-seminars.js
@@ -4,14 +4,4 @@
 // Re-run to refresh:
 //   python fetch_seminars.py --seminar AnJunSem --output-upcoming assets/js/upcoming-ajs-seminars.js
 
-window.UPCOMING_AJS_SEMINARS = [
-  {
-    day: "26",
-    month: "Jun",
-    title: "Polytopal Discontinuous Galerkin Discretizations of Coupled Non-Newtonian Stokes–Darcy Systems",
-    speaker: "Valentina Pederzoli",
-    university: "Politecnico di Milano",
-    meta: "Fri 26/06/2026, 14:00 UTC",
-    website: { href: "https://researchseminars.org/seminar/AnJunSem", label: "researchseminars.org" },
-  },
-];
+window.UPCOMING_AJS_SEMINARS = [];
